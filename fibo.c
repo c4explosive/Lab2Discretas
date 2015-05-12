6c72e6a79c11feb132cbcf27332ec2748f786187
c4explosive <ao946391@gmail.com>
2015-05-11 19:20:12 -0500

    Triangular alrevez

--- fibo.c
+++ fibo.c
@@ -3,15 +3,36 @@
 #include <stdlib.h>
 #define clrscr() system("clear") //Comentar estas dos lineas
 #define getch() system("read")
-//void fiba(int n); //Foldeamos a fiba
 int cont=2; //Contador de fibo
 void fibo(int n1, int n2, int n);
+void trian(int n);
 void main()
 {
     clrscr();
-    fibo(0,1,13);
-    cont=2; //Para la proxima vez que se ejecute
+    /*fibo(0,1,13);
+    printf("\b \n");
+    cont=2; //Para la proxima vez que se ejecute*/
+    trian(5);
+    //getch();
+   
 }
+
+void trian(int n)
+{
+    int acum=0,i;
+    if (n != 0)
+    {
+	for(i=1;i<=n;i++)
+	{
+	    acum+=i;
+	}
+	printf("%d ",acum);
+	n--;
+	trian(n);
+    }
+}
+
+//Fibonacci 
 void fibo(int n1, int n2, int n)
 {
     int res;
@@ -19,14 +40,14 @@ void fibo(int n1, int n2, int n)
     {
     	if (cont==2)
 	{
-	    printf("0 ");
+	    printf("0,");
 	    cont--;
 	    n--;
 	    fibo(n1,n2,n);
 	}
 	else if (cont == 1 )
 	{
-	    printf("1 ");
+	    printf("1,");
 	    cont--;
 	    n--;
 	    fibo(n1,n2,n);
@@ -34,7 +55,7 @@ void fibo(int n1, int n2, int n)
     	else
     	{
 	    res=n1+n2;
-	    printf("%d ",res);
+	    printf("%d,",res);
 	    n--;
 	    fibo(n2,res,n);
         }
